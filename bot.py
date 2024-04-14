f0a236675779d552ee67b20c098fe7ca540ab856
Niall1985 <dcunhaniall@gmail.com>
2024-04-14 23:12:39 +0530

    bot done

--- bot.py
+++ bot.py
@@ -1,9 +1,35 @@
+# import google.generativeai as genai
+# import os
+# from dotenv import load_dotenv
+# import sys
+# load_dotenv()
+# API_KEY = os.getenv('GEMINI_API_KEY')
+# genai.configure(
+#     api_key = API_KEY
+# )
+# model = genai.GenerativeModel('gemini-pro')
+# chat = model.start_chat(history=[])
+# while True:
+#     question = input("You:").lower()
+#     if question == ["quit","exit","break"]:
+#         sys.exit(0)
+
+#     response = chat.send_message(question)
+#     print(f"Bot: {response.text}")
+
+
+
 from flask import Flask, render_template, jsonify, request
-from google.generativeai import genai
+from flask_cors import CORS  # Import CORS extension
+
+import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+# Enable CORS for all routes
+CORS(app)
 
 # Load API key from environment variables
 load_dotenv()
@@ -22,9 +48,7 @@ def index():
     return render_template('index.html')
 
 # Route to handle user input and return bot response
-
-@app.route('/get_response', methods=['POST'])
-
+@app.route('/get_resp', methods=['POST'])
 def get_response():
     # Retrieve user's message from the request
     user_input = request.json['message']
